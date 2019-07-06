fb2d6a4be88475ceb18346f03e4bcbb9301ddefe
realDragonium <beau@tormsed.nl>
2019-07-06 19:24:09 +0200

    commit before pull

--- src/main/java/View/LoginView.java
+++ src/main/java/View/LoginView.java
@@ -9,6 +9,7 @@ import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
@@ -17,7 +18,6 @@ import java.util.TimerTask;
 
 public class LoginView implements LoginObserver{
 
-	public ScrollPane testScrollPane;
 	@FXML
 	private Pane root;
 	@FXML
@@ -40,6 +40,28 @@ public class LoginView implements LoginObserver{
 	public void initialize() {
 		group.getChildren().add(root);
 		loginController.register(this);
+
+		ScrollPane scrollPane = new ScrollPane();
+		root.getChildren().add(scrollPane);
+
+		AnchorPane anchorPane = new AnchorPane();
+		scrollPane.setContent(anchorPane);
+
+		scrollPane.setPrefViewportHeight(220);
+		scrollPane.setPrefViewportWidth(100);
+
+		for (int i = 0; i < 10; i++) {
+			Pane pane = new Pane();
+			Button button = new Button("test"+i);
+			button.setMinHeight(40);
+			button.setMinWidth(80);
+			button.setStyle("-fx-background-color: rgb(46, 59, 69);");
+			pane.getChildren().add(button);
+			pane.setLayoutY(i* 50);
+			pane.setPrefSize(100, 50);
+			anchorPane.getChildren().add(pane);
+		}
+
 	}
 
 	@FXML
