161f8847d2d0eb9512b0a2463e9f8b485ea72098
realDragonium <beau@tormsed.nl>
2019-07-07 15:03:15 +0200

    souts van skeletons power deleted

--- src/main/java/Special/RedeployPhase/SkeletonAction.java
+++ src/main/java/Special/RedeployPhase/SkeletonAction.java
@@ -9,9 +9,7 @@ public class SkeletonAction implements  RedeployPhase, SpecialAction {
 
     @Override
     public void doAction(GameController gameCon, CombinationController combi) {
-        System.out.println("conquered: ");
         int numberOfFiches =  combi.getNumberThisRoundConqueredAreas()/2;
-        System.out.println(numberOfFiches);
         for (int i = 0; i < numberOfFiches ; i++) {
             FicheController ficheCon = new FicheController(1, combi.getRaceName());
             combi.addRaceFiche(ficheCon);
