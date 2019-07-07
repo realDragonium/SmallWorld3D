a7ecca8c95b6a54f0551f736fb9c4cdbeaf357c7
Yoran de Vos <yorandevos12@gmail.com>
2019-07-07 19:10:41 +0200

    Lobby systeem ui aanpassingen

--- src/main/java/View/LobbyView.java
+++ src/main/java/View/LobbyView.java
@@ -9,6 +9,8 @@ import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -22,10 +24,6 @@ public class LobbyView implements LobbyObserver {
     public Group root;
     public Group lobbies;
     public Button back, host;
-    public GridPane panel;
-    private Button activeButton;
-    private int gridCounter = 0;             // startwaarde aantal aangemaakte lobbies = 0
-    private GridPane grid= new GridPane();
     private ScrollPane scrollPane = new ScrollPane();
     private Pane pane = new Pane();
 
@@ -39,10 +37,10 @@ public class LobbyView implements LobbyObserver {
     public void initialize() {
         group.getChildren().add(root);
         lobbyCon.register(this);
-
+        scrollPane.setStyle("-fx-background:rgb(76,76,76);");
         scrollPane.setContent(pane);
-        scrollPane.setPrefViewportHeight(280);
-        scrollPane.setPrefViewportWidth(630);
+        scrollPane.setPrefViewportHeight(450);
+        scrollPane.setPrefViewportWidth(1180);
         lobbies.getChildren().add(scrollPane);
     }
 
@@ -55,23 +53,49 @@ public class LobbyView implements LobbyObserver {
     }
 
     public void setLobby(int index, HashMap lobby){
+
         Pane newLobby = new Pane();
-        newLobby.setPrefWidth(600);
-        newLobby.setPrefHeight(50);
-        newLobby.setBackground(new Background(new BackgroundFill(Color.rgb(200, 200, 200), CornerRadii.EMPTY, Insets.EMPTY)));
-        Font font = new Font(26);
+        ImageView ui = new ImageView();
+        ui.setImage(new Image("/Lobbies/LobbyItemUI" + (index%2 + 1) + ".png"));
+        ui.setFitHeight(80);
+        ui.setFitWidth(1180);
+        newLobby.getChildren().add(ui);
+        newLobby.setPrefWidth(1180);
+        newLobby.setPrefHeight(80);
+
+        Font font = new Font("Algerian", 28);
         Text name = new Text((String)lobby.get("lobbyName"));
+        Text lobbyState = new Text();
+        int size = 0;
+        for(int i = 1; i <= 4; i++){
+            if(!((HashMap)lobby.get("playerNames")).get("player" + i).equals("")) size++;
+        }
+
+        Text players = new Text(size + "/4");
+        if((Boolean)lobby.get("inProgress"))lobbyState.setText("in-Progress");
+        else lobbyState.setText("in-Lobby");
+
+        lobbyState.setLayoutX(560);
+        lobbyState.setLayoutY(50);
+        lobbyState.setFill(Color.WHITE);
+        lobbyState.setFont(font);
         name.setFont(font);
-        name.setLayoutY(20);
-        TextField password = new TextField();
-        password.setLayoutX(200);
+        name.setFill(Color.WHITE);
+        name.setLayoutY(50);
+        name.setLayoutX(60);
+        players.setFont(font);
+        players.setFill(Color.WHITE);
+        players.setLayoutX(860);
+        players.setLayoutY(50);
         Button button = new Button("join");
-        button.setLayoutX(400);
-        button.setOnAction(event -> joinLobby((String)lobby.get("lobbyId"),index, password.getText()));
+        button.setLayoutX(1050);
+        button.setLayoutY(20);
+        button.setOnAction(event -> joinLobby((String)lobby.get("lobbyId"),index, ""));
         newLobby.getChildren().add(name);
-        newLobby.getChildren().add(password);
         newLobby.getChildren().add(button);
-        newLobby.setLayoutY(60 * index);
+        newLobby.getChildren().add(lobbyState);
+        newLobby.getChildren().add(players);
+        newLobby.setLayoutY(80 * index);
         pane.getChildren().add(newLobby);
     }
 
